93f605d3e4d9c9b0bca1bcc678da1839ff5ec205
Edi <gholipourhadi86@gmail.com>
2026-05-16 23:38:20 -0700

    Update src.c

--- src.c
+++ src.c
@@ -1,3 +1,12 @@
+/*
+Taxi System Snap
+Copyright (C) 2026 Edi (Hadi Gholipour)
+
+This program is licensed under the GNU General Public License v3.
+See the LICENSE file or https://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
